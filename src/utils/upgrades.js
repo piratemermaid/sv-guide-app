6a665b37da3cb796d7426f61cc4db83bdb063df6
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-11-26 15:05:03 -0500

    Add upgrade list

--- src/utils/upgrades.js
+++ src/utils/upgrades.js
@@ -1,1 +1,171 @@
-export const upgradeIds = [""];
+export const upgradeList = [
+  {
+    name: "Backpack Upgrade 1",
+    cost: []
+  },
+  { name: "Backpack Upgrade 2", cost: [] },
+  {
+    name: "Copper Pickaxe",
+    cost: []
+  },
+  {
+    name: "Steel Pickaxe",
+    cost: []
+  },
+  {
+    name: "Gold Pickaxe",
+    cost: []
+  },
+  {
+    name: "Platinum Pickaxe",
+    cost: []
+  },
+  {
+    name: "Copper Watering Can",
+    cost: []
+  },
+  {
+    name: "Steel Watering Can",
+    cost: []
+  },
+  {
+    name: "Gold Watering Can",
+    cost: []
+  },
+  {
+    name: "Platinum Watering Can",
+    cost: []
+  },
+  {
+    name: "Copper Axe",
+    cost: []
+  },
+  {
+    name: "Steel Axe",
+    cost: []
+  },
+  {
+    name: "Gold Axe",
+    cost: []
+  },
+  {
+    name: "Platinum Axe",
+    cost: []
+  },
+  {
+    name: "Copper Hoe",
+    cost: []
+  },
+  {
+    name: "Steel Hoe",
+    cost: []
+  },
+  {
+    name: "Gold Hoe",
+    cost: []
+  },
+  {
+    name: "Platinum Hoe",
+    cost: []
+  },
+  {
+    name: "Well",
+    cost: []
+  },
+  {
+    name: "Coop",
+    cost: []
+  },
+  {
+    name: "Big Coop",
+    cost: []
+  },
+  {
+    name: "Deluxe Coop",
+    cost: []
+  },
+  {
+    name: "Barn",
+    cost: []
+  },
+  {
+    name: "Big Barn",
+    cost: []
+  },
+  {
+    name: "Deluxe Barn",
+    cost: []
+  },
+  {
+    name: "Mill",
+    cost: []
+  },
+  {
+    name: "Stable",
+    cost: []
+  },
+  {
+    name: "Shed",
+    cost: []
+  },
+  {
+    name: "Chicken",
+    cost: []
+  },
+  {
+    name: "Duck",
+    cost: []
+  },
+  {
+    name: "Rabbit",
+    cost: []
+  },
+  {
+    name: "Cow",
+    cost: []
+  },
+  {
+    name: "Goat",
+    cost: []
+  },
+  {
+    name: "Sheep",
+    cost: []
+  },
+  {
+    name: "Pig",
+    cost: []
+  },
+  {
+    name: "Milk Pail",
+    cost: []
+  },
+  {
+    name: "Shears",
+    cost: []
+  },
+  {
+    name: "Peach Tree",
+    cost: []
+  },
+  {
+    name: "Cherry Tree",
+    cost: []
+  },
+  {
+    name: "Apricot Tree",
+    cost: []
+  },
+  {
+    name: "Orange Tree",
+    cost: []
+  },
+  {
+    name: "Apple Tree",
+    cost: []
+  },
+  {
+    name: "Pomegranate Tree",
+    cost: []
+  }
+];
