3c58cbb173f289f0a8241e17d6b14f84cdf101b4
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-11-26 15:47:33 -0500

    Work on CC

--- src/components/CCItem.js
+++ src/components/CCItem.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const CCItem = props => {
-  return <div>{props.info.name}</div>;
+  const { name, season, location, time, special } = props.info;
+
+  function renderSeasons(seasons) {
+    return seasons.map(season => {
+      return <div className={`season-dot ${season}`} />;
+    });
+  }
+
+  return (
+    <div>
+      {name}
+      {season.constructor === Array ? renderSeasons(season) : null}
+    </div>
+  );
 };
 
 export default CCItem;
