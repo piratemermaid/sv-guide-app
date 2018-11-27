ecdaa8c621de0b363491ce3130b27d5444411f5d
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-11-27 16:27:05 -0500

    Update upgrade costs

--- src/utils/upgrades.js
+++ src/utils/upgrades.js
@@ -1,80 +1,84 @@
 export const upgradeItems = [
   {
     name: "Backpack Upgrade 1",
-    cost: []
+    cost: [{ Gold: 2000 }]
   },
-  { name: "Backpack Upgrade 2", cost: [] },
+  { name: "Backpack Upgrade 2", cost: [{ Gold: 10000 }] },
   {
     name: "Milk Pail",
-    cost: []
+    cost: [{ Gold: 1000 }]
   },
   {
     name: "Shears",
-    cost: []
+    cost: [{ Gold: 1000 }]
+  },
+  {
+    name: "Auto-Grabber",
+    cost: [{ Gold: 25000 }]
   },
   {
     name: "Copper Pickaxe",
-    cost: [{ "Copper Bar": 5 }]
+    cost: [{ "Copper Bar": 5 }, { Gold: 2000 }]
   },
   {
     name: "Steel Pickaxe",
-    cost: []
+    cost: [{ "Iron Bar": 5 }, { Gold: 5000 }]
   },
   {
     name: "Gold Pickaxe",
-    cost: []
+    cost: [{ "Gold Bar": 5 }, { Gold: 10000 }]
   },
   {
     name: "Iridium Pickaxe",
-    cost: []
+    cost: [{ "Iridium Bar": 5 }, { Gold: 25000 }]
   },
   {
     name: "Copper Watering Can",
-    cost: [{ "Copper Bar": 5 }]
+    cost: [{ "Copper Bar": 5 }, { Gold: 2000 }]
   },
   {
     name: "Steel Watering Can",
-    cost: []
+    cost: [{ "Iron Bar": 5 }, { Gold: 5000 }]
   },
   {
     name: "Gold Watering Can",
-    cost: []
+    cost: [{ "Gold Bar": 5 }, { Gold: 10000 }]
   },
   {
     name: "Iridium Watering Can",
-    cost: []
+    cost: [{ "Iridium Bar": 5 }, { Gold: 25000 }]
   },
   {
     name: "Copper Axe",
-    cost: [{ "Copper Bar": 5 }]
+    cost: [{ "Copper Bar": 5 }, { Gold: 2000 }]
   },
   {
     name: "Steel Axe",
-    cost: []
+    cost: [{ "Iron Bar": 5 }, { Gold: 5000 }]
   },
   {
     name: "Gold Axe",
-    cost: []
+    cost: [{ "Gold Bar": 5 }, { Gold: 10000 }]
   },
   {
     name: "Iridium Axe",
-    cost: []
+    cost: [{ "Iridium Bar": 5 }, { Gold: 25000 }]
   },
   {
     name: "Copper Hoe",
-    cost: [{ "Copper Bar": 5 }]
+    cost: [{ "Copper Bar": 5 }, { Gold: 2000 }]
   },
   {
     name: "Steel Hoe",
-    cost: []
+    cost: [{ "Iron Bar": 5 }, { Gold: 5000 }]
   },
   {
     name: "Gold Hoe",
-    cost: []
+    cost: [{ "Gold Bar": 5 }, { Gold: 10000 }]
   },
   {
     name: "Iridium Hoe",
-    cost: []
+    cost: [{ "Iridium Bar": 5 }, { Gold: 25000 }]
   },
   {
     name: "Well",
@@ -171,8 +175,8 @@ export const upgradeItems = [
 ];
 
 export const upgradeGroups = {
-  tools: [0, 19],
-  buildings: [20, 29],
-  animals: [30, 36],
-  trees: [37, 42]
+  tools: [0, 20],
+  buildings: [21, 30],
+  animals: [31, 37],
+  trees: [38, 43]
 };
