6aebb0f5c87f57775f615a13410ea49432cb34b9
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-11-28 15:41:21 -0500

    Add calendar events

---
+++ src/utils/calendar.js
@@ -0,0 +1,51 @@
+export default (calendarEvents = {
+  spring: [
+    { day: 4, name: "Kent", type: "birthday", loves: [], likes: [] },
+    { day: 7, name: "Lewis", type: "birthday", loves: [], likes: [] },
+    { day: 10, name: "Vincent", type: "birthday", loves: [], likes: [] },
+    { day: 13, name: "Egg Festival", type: "event" },
+    { day: 14, name: "Haley", type: "birthday", loves: [], likes: [] },
+    { day: 14, name: "Haley", type: "birthday", loves: [], likes: [] },
+    { day: 14, name: "Haley", type: "birthday", loves: [], likes: [] },
+    { day: 13, name: "Flower Dance", type: "event" },
+    { day: 14, name: "Haley", type: "birthday", loves: [], likes: [] },
+    { day: 14, name: "Haley", type: "birthday", loves: [], likes: [] }
+  ],
+  summer: [
+    { day: 4, name: "Jas", type: "birthday", loves: [], likes: [] },
+    { day: 8, name: "Gus", type: "birthday", loves: [], likes: [] },
+    { day: 10, name: "Maru", type: "birthday", loves: [], likes: [] },
+    { day: 11, name: "Luau", type: "event", options: { good: [], great: [] } },
+    { day: 13, name: "Alex", type: "birthday", loves: [], likes: [] },
+    { day: 17, name: "Sam", type: "birthday", loves: [], likes: [] },
+    { day: 19, name: "Demetrius", type: "birthday", loves: [], likes: [] },
+    { day: 22, name: "Dwarf", type: "birthday", loves: [], likes: [] },
+    { day: 24, name: "Willy", type: "birthday", loves: [], likes: [] },
+    { day: 28, name: "Moonlight Jellies", type: "event" }
+  ],
+  fall: [
+    { day: 2, name: "Penny", type: "birthday", loves: [], likes: [] },
+    { day: 5, name: "Elliott", type: "birthday", loves: [], likes: [] },
+    { day: 11, name: "Jodi", type: "birthday", loves: [], likes: [] },
+    { day: 13, name: "Abigail", type: "birthday", loves: [], likes: [] },
+    { day: 15, name: "Sandy", type: "birthday", loves: [], likes: [] },
+    { day: 16, name: "Stardew Valley Fair", type: "event", items: [] },
+    { day: 18, name: "Marnie", type: "birthday", loves: [], likes: [] },
+    { day: 21, name: "Robin", type: "birthday", loves: [], likes: [] },
+    { day: 24, name: "George", type: "birthday", loves: [], likes: [] },
+    { day: 27, name: "Spirit's Eve Festival", type: "event" }
+  ],
+  winter: [
+    { day: 1, name: "Krobus", type: "birthday", loves: [], likes: [] },
+    { day: 3, name: "Linus", type: "birthday", loves: [], likes: [] },
+    { day: 7, name: "Caroline", type: "birthday", loves: [], likes: [] },
+    { day: 8, name: "Festival of Ice", type: "event" },
+    { day: 10, name: "Sebastian", type: "birthday", loves: [], likes: [] },
+    { day: 14, name: "Harvey", type: "birthday", loves: [], likes: [] },
+    { day: 17, name: "The Wizard", type: "birthday", loves: [], likes: [] },
+    { day: 20, name: "Evelyn", type: "birthday", loves: [], likes: [] },
+    { day: 23, name: "Leah", type: "birthday", loves: [], likes: [] },
+    { day: 25, name: "Flower Dance", type: "event" },
+    { day: 26, name: "Clint", type: "birthday", loves: [], likes: [] }
+  ]
+});
