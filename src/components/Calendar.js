e7d4b8d2d912e55e81030deadb90adb1ec7fccdc
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-11-28 16:17:53 -0500

    Add Calendar UI

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,13 +4,40 @@ import { calendarEvents } from "../utils/calendar";
 import SeasonFilterBtns from "./SeasonFilterBtns";
 
 const Calendar = props => {
+  function renderCalendarItems(season) {
+    if (props.seasonFilter && season !== props.seasonFilter) {
+      return;
+    }
+
+    const eventList = calendarEvents[season.toLowerCase()];
+
+    return (
+      <div>
+        <h3 className={season.toLowerCase()}>{season}</h3>
+        {eventList.map(event => {
+          console.log(event);
+          return (
+            <p key={event.name}>
+              <b>{event.day}</b>: {event.name}
+              {event.type === "birthday" ? "'s birthday" : null}
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h3>Calendar</h3>
+      <h2>Calendar</h2>
       <SeasonFilterBtns
         seasonFilter={props.seasonFilter}
         changeSeasonFilter={props.changeSeasonFilter}
       />
+      {renderCalendarItems("Spring")}
+      {renderCalendarItems("Summer")}
+      {renderCalendarItems("Fall")}
+      {renderCalendarItems("Winter")}
     </div>
   );
 };
