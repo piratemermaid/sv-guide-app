1b413dc6ed515f4805567fdbbe4fd2054dece270
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2019-07-26 14:58:41 -0400

    Reformat CC item text

--- src/components/CCItem.js
+++ src/components/CCItem.js
@@ -3,45 +3,65 @@ import React from "react";
 import { getImgUrl } from "../utils/utils";
 
 const CCItem = props => {
-  const { name, season, amt, location, time, special } = props.info;
+    const { name, season, amt, location, time, special } = props.info;
 
-  function renderSeasons(seasons) {
-    return seasons.map(season => {
-      return <div key={season} className={`season-dot ${season}`} />;
-    });
-  }
+    function renderSeasons(seasons) {
+        return seasons.map(season => {
+            return <div key={season} className={`season-dot ${season}`} />;
+        });
+    }
 
-  function handleCheck(id) {
-    props.toggleItem(id, "cc");
-  }
+    function handleCheck(id) {
+        props.toggleItem(id, "cc");
+    }
 
-  return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={props.checked ? "checked" : ""}
-          onChange={() => handleCheck(props.id, name)}
-        />
-        <span style={{ fontSize: "13px" }}>
-          <img
-            src={`/img/${getImgUrl(name)}.png`}
-            title={name}
-            alt={name}
-            className="ccItem-img"
-          />
-          {name.includes(" g") ? ` ${name}` : null}
-          {amt ? ` x${amt}` : null}
-          {location ? location : null}
-          {location && time ? " " : null}
-          {time ? time : null}
-          {time && special ? " " : null}
-          {special ? `(${special})` : null}
-        </span>
-      </label>
-      {season.constructor === Array ? renderSeasons(season) : null}
-    </div>
-  );
+    let itemText = name;
+    if (amt) {
+        itemText += ` x${amt}`;
+    }
+    if (location || time || special) {
+        itemText += " (";
+    }
+    if (location) {
+        itemText += `${location}`;
+    }
+    if (time) {
+        if (location) {
+            itemText += ", ";
+        }
+        itemText += ` ${time}`;
+    }
+    if (special) {
+        if (location || time) {
+            itemText += ", ";
+        }
+        itemText += ` ${special}`;
+    }
+    if (location || time || special) {
+        itemText += ")";
+    }
+
+    return (
+        <div>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={props.checked ? "checked" : ""}
+                    onChange={() => handleCheck(props.id, name)}
+                />
+                <span style={{ fontSize: "13px" }}>
+                    <img
+                        src={`/img/${getImgUrl(name)}.png`}
+                        title={name}
+                        alt={name}
+                        className="ccItem-img"
+                    />
+                    {itemText}
+                </span>
+            </label>
+            {season.constructor === Array ? renderSeasons(season) : null}
+        </div>
+    );
 };
 
 export default CCItem;
