92d6487e7f03564aa1390fadcbf6d1bb92179dc2
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2019-09-25 13:40:32 -0400

    update package jsons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,24 +4,26 @@ import { Link } from "react-router-dom";
 import { URLS } from "../utils/globals";
 
 const Header = () => {
-  return (
-    <div>
-      <ul className="sidenav yellow lighten-4">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to={URLS.UPGRADES}>Upgrades</Link>
-        </li>
-        <li>
-          <Link to={URLS.COMMUNITY_CENTER}>Community Center</Link>
-        </li>
-        <li>
-          <Link to={URLS.CALENDAR}>Calendar</Link>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <ul className="yellow lighten-4">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to={URLS.UPGRADES}>Upgrades</Link>
+                    </li>
+                    <li>
+                        <Link to={URLS.COMMUNITY_CENTER}>Community Center</Link>
+                    </li>
+                    <li>
+                        <Link to={URLS.CALENDAR}>Calendar</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Header;
