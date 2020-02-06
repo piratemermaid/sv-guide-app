98fc1e757d943007a97cfd56466e71d541686864
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2020-02-06 09:52:27 -0500

    knex db setup

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,44 +1,54 @@
-// Update with your config settings.
+const { DB_DEV, DB_PROD } = require("./secrets");
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "pg",
     connection: {
-      filename: './dev.sqlite3'
+      host: DB_DEV.host,
+      user: DB_DEV.user,
+      password: DB_DEV.password,
+      database: DB_DEV.database,
+      charset: "utf8"
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
     }
   },
 
   staging: {
-    client: 'postgresql',
+    client: "pg",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: "pg",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: DB_PROD.host,
+      user: DB_PROD.user,
+      password: DB_PROD.password,
+      database: DB_PROD.database,
+      charset: "utf8"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
