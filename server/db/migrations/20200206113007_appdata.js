cb353d3cf1eecc3c91ec002402392d3bf031051e
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2020-02-06 13:40:44 -0500

    add user data tables

--- server/db/migrations/20200206113007_appdata.js
+++ server/db/migrations/20200206113007_appdata.js
@@ -1,6 +1,9 @@
 const TABLES = require("../tables");
 
 exports.up = async function(knex) {
+  /////////////////////////////////////////
+  // app data tables
+  /////////////////////////////////////////
   await knex.schema.createTable(TABLES.ROOMS, table => {
     table.increments("id");
     table.string("name").notNullable();
@@ -50,17 +53,143 @@ exports.up = async function(knex) {
     table.jsonb("cost");
     table.string("prereq");
   });
+
+  /////////////////////////////////////////
+  // user tables
+  /////////////////////////////////////////
+  await knex.schema.createTable(TABLES.USERS, table => {
+    table
+      .increments("id")
+      .unsigned()
+      .primary();
+    table
+      .string("username")
+      .unique()
+      .notNullable();
+    table.string("password").notNullable();
+    table.string("email").notNullable();
+    table.string("sessionId");
+  });
+
+  await knex.schema.createTable(TABLES.CHARACTERS, table => {
+    table
+      .increments("id")
+      .unsigned()
+      .primary();
+    table.string("name").notNullable();
+  });
+
+  await knex.schema.createTable(TABLES.USERS_CHARACTERS, table => {
+    table
+      .increments("id")
+      .unsigned()
+      .primary();
+    table
+      .integer("user_id")
+      .references("id")
+      .inTable(TABLES.USERS)
+      .onDelete("cascade")
+      .notNullable();
+    table
+      .integer("character_id")
+      .references("id")
+      .inTable(TABLES.CHARACTERS)
+      .onDelete("cascade")
+      .notNullable();
+  });
+
+  await knex.schema.createTable(TABLES.CHARACTERS_ROOMS, table => {
+    table
+      .increments("id")
+      .unsigned()
+      .primary();
+    table
+      .integer("character_id")
+      .references("id")
+      .inTable(TABLES.CHARACTERS)
+      .onDelete("cascade")
+      .notNullable();
+    table
+      .integer("room_id")
+      .references("id")
+      .inTable(TABLES.ROOMS)
+      .onDelete("cascade")
+      .notNullable();
+  });
+
+  await knex.schema.createTable(TABLES.CHARACTERS_BUNDLES, table => {
+    table
+      .increments("id")
+      .unsigned()
+      .primary();
+    table
+      .integer("character_id")
+      .references("id")
+      .inTable(TABLES.CHARACTERS)
+      .onDelete("cascade")
+      .notNullable();
+    table
+      .integer("bundle_id")
+      .references("id")
+      .inTable(TABLES.BUNDLES)
+      .onDelete("cascade")
+      .notNullable();
+  });
+
+  await knex.schema.createTable(TABLES.CHARACTERS_BUNDLE_ITEMS, table => {
+    table
+      .increments("id")
+      .unsigned()
+      .primary();
+    table
+      .integer("character_id")
+      .references("id")
+      .inTable(TABLES.CHARACTERS)
+      .onDelete("cascade")
+      .notNullable();
+    table
+      .integer("bundle_item_id")
+      .references("id")
+      .inTable(TABLES.BUNDLE_ITEMS)
+      .onDelete("cascade")
+      .notNullable();
+  });
+
+  await knex.schema.createTable(TABLES.CHARACTERS_UPGRADES, table => {
+    table
+      .increments("id")
+      .unsigned()
+      .primary();
+    table
+      .integer("character_id")
+      .references("id")
+      .inTable(TABLES.CHARACTERS)
+      .onDelete("cascade")
+      .notNullable();
+    table
+      .integer("upgrade_id")
+      .references("id")
+      .inTable(TABLES.UPGRADES)
+      .onDelete("cascade")
+      .notNullable();
+  });
 };
 
 exports.down = async function(knex) {
   const tableOrder = [
-    TABLES.USERS,
     TABLES.ROOMS,
     TABLES.BUNDLES,
     TABLES.BUNDLE_ITEMS,
-    TABLES.UPGRADES
+    TABLES.UPGRADES,
+    TABLES.USERS,
+    TABLES.CHARACTERS,
+    TABLES.USERS_CHARACTERS,
+    TABLES.CHARACTERS_ROOMS,
+    TABLES.CHARACTERS_BUNDLES,
+    TABLES.CHARACTERS_BUNDLE_ITEMS,
+    TABLES.CHARACTERS_UPGRADES
   ];
-  for (let i = tableOrder.length; i > 0; i++) {
+  for (let i = tableOrder.length; i >= 0; i--) {
     await knex.schema.dropTableIfExists(tableOrder[i]);
   }
 };
--- server/db/tables.js
+++ server/db/tables.js
@@ -1,4 +1,11 @@
 const TABLES = {
+  USERS: "users",
+  CHARACTERS: "characters",
+  USERS_CHARACTERS: "users_characters",
+  CHARACTERS_ROOMS: "characters_rooms",
+  CHARACTERS_BUNDLES: "characters_bundles",
+  CHARACTERS_BUNDLE_ITEMS: "characters_bundle_items",
+  CHARACTERS_UPGRADES: "characters_upgrades",
   ROOMS: "rooms",
   BUNDLES: "bundles",
   BUNDLE_ITEMS: "bundle_items",
