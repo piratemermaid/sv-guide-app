59317de4b9f48a2830683dd31ea69e2a28811747
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2020-02-06 14:03:44 -0500

    add authenticated status to front end

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Home = () => {
-  return <h3>Home</h3>;
+// TODO: show list of user characters & allow them
+// to switch or create new
+const Home = props => {
+  return (
+    <div className="container">
+      <h3>Home</h3>
+      {props.authenticated ? "hi user" : "login or signup"}
+      {/* <a onClick={}>Signup</a> */}
+      {/* {" | "} */}
+      {/* <a onClick={}>Login</a> */}
+    </div>
+  );
 };
 
 export default Home;
