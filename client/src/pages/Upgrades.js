6eb59f70e495031a65b360d00f23be4213a67c71
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2020-02-10 11:27:43 -0500

    upgrades FE

--- client/src/pages/Upgrades.js
+++ client/src/pages/Upgrades.js
@@ -1,36 +1,10 @@
 import _ from "lodash";
 import React from "react";
 
-import { upgradeItems, upgradeGroups } from "../utils/upgrades";
-import { getItemID } from "../utils/utils";
-import UpgradeItem from "../components/UpgradeItem";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const Upgrades = props => {
-  function renderUpgrades(group) {
-    const range = upgradeGroups[group];
-    let arr = [];
-    for (let i = range[0]; i <= range[1]; i++) {
-      const id = getItemID(upgradeItems[i].name, "upgrades");
-      let prereqID;
-      if (upgradeItems[i].prereq) {
-        prereqID = getItemID(upgradeItems[i].prereq, "upgrades");
-      }
-
-      arr.push(
-        <UpgradeItem
-          key={id}
-          info={upgradeItems[i]}
-          checked={props.upgrades[id] === 1}
-          toggleItem={props.toggleItem}
-          id={id}
-          prereqStatus={props.upgrades[prereqID]}
-        />
-      );
-    }
-
-    return arr;
-  }
-
   function getDay(day) {
     let dayClass = "";
     if (props.toolPickup === day) {
@@ -58,85 +32,50 @@ const Upgrades = props => {
     }
   }
 
-  function renderUpgrades(upgrades) {
-    console.log(upgrades);
-    // let ui = [];
-    // for (let type in upgrades) {
-    //   console.log(i, upgrades[i]);
-    //   //   const {name,type,}=(upgrades[i]);
-    // }
-  }
-
   const upgradesByType = _.groupBy(props.upgrades, "type");
+
+  const handleChange = (e, name) => {
+    console.log(e.target.checked, name);
+  };
 
   return (
     <div>
-      <div>
-        <h2>
-          <span className="left">Upgrades</span>
-          <span
-            className="new badge left"
-            onClick={() => props.reset("upgrades")}
-          >
-            Reset
-          </span>
-        </h2>
-      </div>
       {_.map(upgradesByType, typeUpgrades => {
         const type = typeUpgrades[0].type;
         return (
           <div key={type}>
             <div>
-              <h3>{type}</h3>
+              <h3>
+                {type}
+                {type !== "home" ? "s" : null}
+              </h3>
               <ul>
                 {typeUpgrades.map(({ name, cost, prereq }) => {
-                  return <li key={name}>{name} </li>;
+                  // TODO: get checked & prereqStatus from userData
+                  const checked = false;
+                  const prereqStatus = false;
+
+                  return (
+                    <li key={name}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={checked && prereqStatus}
+                            onChange={e => handleChange(e, name)}
+                            value={name}
+                          />
+                        }
+                        label={name}
+                      />
+                    </li>
+                  );
                 })}
               </ul>
             </div>
           </div>
         );
       })}
-      {/* {renderUpgrades(upgradesByType)} */}
     </div>
-    // <div>
-    //     <div >
-    // <h2>
-    //     <span className="left">Upgrades</span>
-    //     <span
-    //         className="new badge left"
-    //         onClick={() => props.reset("upgrades")}
-    //     >
-    //         Reset
-    //     </span>
-    // </h2>
-    //     </div>
-    // <div className="row">
-    //     <div className="col s6">
-    //             <h3>Tools</h3>
-    //             <div className="tool-pickup">
-    //                 Pickup: {getDay("SU")}
-    //                 {getDay("M")}
-    //                 {getDay("TU")}
-    //                 {getDay("W")}
-    //                 {getDay("TH")}
-    //                 {getDay("F")}
-    //                 {getDay("SA")}
-    //             </div>
-    //             {renderUpgrades("tools")}
-    //             <h3>Home</h3>
-    //             {renderUpgrades("home")}
-    //         </div>
-    //         <div className="col s6">
-    //             <h3>Buildings</h3>
-    //             {renderUpgrades("buildings")}
-    //             <h3>Animals</h3>
-    //             {renderUpgrades("animals")}
-    //             <h3>Trees</h3>
-    //             {renderUpgrades("trees")}
-    //         </div>
-    //     </div>
-    // </div>
   );
 };
 
