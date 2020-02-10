17c7aebd4640bda0e27334175ce30a21fcf630ac
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2020-02-10 09:35:41 -0500

    add sidenav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,29 +1,39 @@
 import React from "react";
+import M from "materialize-css";
 import { Link } from "react-router-dom";
 
 import { URLS } from "../utils/globals";
 
-const Header = () => {
+class Header extends React.Component {
+  componentDidMount() {
+    var elems = document.querySelectorAll(".sidenav");
+    var options = { closeOnClick: true };
+    var instances = M.Sidenav.init(elems, options);
+  }
+
+  render() {
     return (
-        <nav>
-            <div className="nav-wrapper">
-                <ul className="yellow lighten-4">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to={URLS.UPGRADES}>Upgrades</Link>
-                    </li>
-                    <li>
-                        <Link to={URLS.COMMUNITY_CENTER}>Community Center</Link>
-                    </li>
-                    <li>
-                        <Link to={URLS.CALENDAR}>Calendar</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+      <div>
+        <ul id="slide-out" className="sidenav sidenav-fixed">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to={URLS.UPGRADES}>Upgrades</Link>
+          </li>
+          <li>
+            <Link to={URLS.COMMUNITY_CENTER}>Community Center</Link>
+          </li>
+          <li>
+            <Link to={URLS.CALENDAR}>Calendar</Link>
+          </li>
+        </ul>
+        <a data-target="slide-out" className="sidenav-trigger">
+          <i className="material-icons">menu</i>
+        </a>
+      </div>
     );
-};
+  }
+}
 
 export default Header;
