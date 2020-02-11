53d242c4c0663b372a27e89941f5caf053abf165
piratemermaid <schnur.ariel@gmail.com>
2020-02-11 08:54:08 -0500

    show loading spinner while importing

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,54 +1,70 @@
-import React from "react";
-import axios from "axios";
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-const Account = props => {
-  if (!props.appData) {
-    return "Loading...";
+class Account extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { importing: false };
   }
 
-  const saveToLS = () => {
-    localStorage.setItem("svData", JSON.stringify(props.characters));
+  saveToLS = () => {
+    localStorage.setItem("svData", JSON.stringify(this.props.characters));
   };
 
-  const importFromLS = async () => {
+  importFromLS = async () => {
+    this.setState({ importing: true });
+
     const characters = JSON.parse(localStorage.getItem("svData"));
 
     for (let i in characters) {
       const { name, upgrades, rooms, bundles, bundleItems } = characters[i];
 
-      await props.addCharacter(name);
+      await this.props.addCharacter(name);
 
       for (let upgrade of upgrades) {
         const { name } = upgrade;
-        await props.toggleUpgrade({ upgradeName: name, value: true });
+        await this.props.toggleUpgrade({ upgradeName: name, value: true });
       }
 
       for (let room of rooms) {
         const { name } = room;
-        await props.toggleRoom({ name, value: true });
+        await this.props.toggleRoom({ name, value: true });
       }
 
       for (let bundle of bundles) {
         const { name } = bundle;
-        await props.toggleBundle({ name, value: true });
+        await this.props.toggleBundle({ name, value: true });
       }
 
       for (let item of bundleItems) {
         const { key } = item;
-        await props.toggleBundleItem({ key, value: true });
+        await this.props.toggleBundleItem({ key, value: true });
       }
     }
 
-    alert("import complete");
+    this.setState({ importing: false });
   };
 
-  return (
-    <div>
-      <Button onClick={() => saveToLS()}>Save Data to LS</Button>
-      <Button onClick={() => importFromLS()}>Import Data from LS</Button>
-    </div>
-  );
-};
+  render() {
+    if (!this.props.appData) {
+      return <CircularProgress />;
+    } else {
+      return (
+        <div>
+          {!this.state.importing ? (
+            <div>
+              <Button onClick={this.saveToLS}>Save Data to LS</Button>
+              <Button onClick={this.importFromLS}>Import Data from LS</Button>
+            </div>
+          ) : (
+            <CircularProgress />
+          )}
+        </div>
+      );
+    }
+  }
+}
 
 export default Account;
