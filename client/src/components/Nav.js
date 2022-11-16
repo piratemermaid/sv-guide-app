d3764a7243477a00b1bdcca558e677a0894fea86
piratemermaid <schnur.ariel@gmail.com>
2022-11-16 13:45:23 -0700

    Update Nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,133 +1,64 @@
-import React from "react";
+import * as React from "react";
 import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import {
+  Box,
+  Typography,
+  Toolbar,
+  List,
+  CssBaseline,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon
+} from "@mui/material";
+import {
+  Menu as MenuIcon,
+  ChevronLeft as ChevronLeftIcon,
+  ChevronRight as ChevronRightIcon,
+  SupervisorAccount as SupervisorAccountIcon,
+  ArrowUpward as ArrowUpwardIcon,
+  Store as StoreIcon,
+  Today as TodayIcon,
+  AccountBox as AccountBoxIcon
+} from "@mui/icons-material";
+
+import { Drawer, DrawerHeader, AppBar } from "./NavComponents";
 import { URLS } from "../utils/globals";
 
-// import { useTheme } from "@material-ui/core/styles";
-// import Drawer from "@material-ui/core/Drawer";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import List from "@material-ui/core/List";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import Typography from "@material-ui/core/Typography";
-// import Divider from "@material-ui/core/Divider";
-// import IconButton from "@material-ui/core/IconButton";
-// import MenuIcon from "@material-ui/icons/Menu";
-// import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-// import ListItemText from "@material-ui/core/ListItemText";
-// import InboxIcon from "@material-ui/icons/MoveToInbox";
-// import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
-// import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-// import StoreIcon from "@material-ui/icons/Store";
-// import TodayIcon from "@material-ui/icons/Today";
-// import AccountBoxIcon from "@material-ui/icons/AccountBox";
-
-const drawerWidth = 240;
-
-const styles = {
-  // root: {
-  //   display: "flex"
-  // },
-  // appBar: {
-  //   zIndex: theme.zIndex.drawer + 1,
-  //   transition: theme.transitions.create(["width", "margin"], {
-  //     easing: theme.transitions.easing.sharp,
-  //     duration: theme.transitions.duration.leavingScreen
-  //   })
-  // },
-  // appBarShift: {
-  //   marginLeft: drawerWidth,
-  //   width: `calc(100% - ${drawerWidth}px)`,
-  //   transition: theme.transitions.create(["width", "margin"], {
-  //     easing: theme.transitions.easing.sharp,
-  //     duration: theme.transitions.duration.enteringScreen
-  //   })
-  // },
-  // menuButton: {
-  //   marginRight: 36
-  // },
-  // hide: {
-  //   display: "none"
-  // },
-  // drawer: {
-  //   width: drawerWidth,
-  //   flexShrink: 0,
-  //   whiteSpace: "nowrap"
-  // },
-  // drawerOpen: {
-  //   width: drawerWidth,
-  //   transition: theme.transitions.create("width", {
-  //     easing: theme.transitions.easing.sharp,
-  //     duration: theme.transitions.duration.enteringScreen
-  //   })
-  // },
-  // drawerClose: {
-  //   transition: theme.transitions.create("width", {
-  //     easing: theme.transitions.easing.sharp,
-  //     duration: theme.transitions.duration.leavingScreen
-  //   }),
-  //   overflowX: "hidden",
-  //   width: theme.spacing(7) + 1,
-  //   [theme.breakpoints.up("sm")]: {
-  //     width: theme.spacing(9) + 1
-  //   }
-  // },
-  // toolbar: {
-  //   display: "flex",
-  //   alignItems: "center",
-  //   justifyContent: "flex-end",
-  //   padding: theme.spacing(0, 1),
-  //   ...theme.mixins.toolbar
-  // },
-  // content: {
-  //   flexGrow: 1,
-  //   padding: theme.spacing(3)
-  // }
-};
-
-const getIcon = (text) => {
-  switch (text) {
-    case "Characters":
-      return <SupervisorAccountIcon />;
-    case "Upgrades":
-      return <ArrowUpwardIcon />;
-    case "Community Center":
-      return <StoreIcon />;
-    case "My Account":
-      return <AccountBoxIcon />;
-    case "Calendar":
-      return <TodayIcon />;
-    default:
-      console.log("need icon for", text);
-      return <InboxIcon />;
+const navList = [
+  {
+    text: "Characters",
+    url: URLS.Characters,
+    icon: <SupervisorAccountIcon />
+  },
+  {
+    text: "Upgrades",
+    url: URLS.Upgrades,
+    icon: <ArrowUpwardIcon />
+  },
+  {
+    text: "Community Center",
+    url: URLS["Community Center"],
+    icon: <StoreIcon />
+  },
+  {
+    text: "Calendar",
+    url: URLS.Calendar,
+    icon: <TodayIcon />
+  },
+  {
+    text: "My Account",
+    url: URLS["My Account"],
+    icon: <AccountBoxIcon />
   }
-};
+];
 
-const getPageName = (path) => {
-  switch (path) {
-    case "/":
-      return "Characters";
-    case "/upgrades":
-      return "Upgrades";
-    case "/community_center":
-      return "Community Center";
-    case "/account":
-      return "My Account";
-    case "/calendar":
-      return "Calendar";
-    case "/login":
-      return "Log In";
-    case "/signup":
-      return "Sign Up";
-    default:
-      console.log("need page name for", path);
-      return "";
-  }
-};
-
-const Header = (props) => {
+const Nav = () => {
+  const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -138,60 +69,69 @@ const Header = (props) => {
     setOpen(false);
   };
 
-  return <div>NAV TODO</div>;
-
-  // return (
-  //   <div sx={styles.root}>
-  //     <CssBaseline />
-  //     <AppBar position="fixed" className={styles.appBar}>
-  //       <Toolbar>
-  //         <IconButton
-  //           color="inherit"
-  //           aria-label="open drawer"
-  //           onClick={handleDrawerOpen}
-  //           edge="start"
-  //           sx={styles.menuButton}
-  //         >
-  //           <MenuIcon />
-  //         </IconButton>
-  //         <Typography variant="h6" noWrap>
-  //           Stardew Tracker | {getPageName(props.location.pathname)}
-  //         </Typography>
-  //       </Toolbar>
-  //     </AppBar>
-  //     <Drawer variant="permanent">
-  //       <div sx={styles.toolbar}>
-  //         <IconButton onClick={handleDrawerClose}>
-  //           {theme.direction === "rtl" ? (
-  //             <ChevronRightIcon />
-  //           ) : (
-  //             <ChevronLeftIcon />
-  //           )}
-  //         </IconButton>
-  //       </div>
-  //       <Divider />
-  //       <List>
-  //         {["Characters", "Upgrades", "Community Center", "Calendar"].map(
-  //           (text) => (
-  //             <ListItem href={URLS[text]} button key={text} component="a">
-  //               <ListItemIcon>{getIcon(text)}</ListItemIcon>
-  //               <ListItemText primary={text} />
-  //             </ListItem>
-  //           )
-  //         )}
-  //       </List>
-  //       <Divider />
-  //       <List>
-  //         {["My Account"].map((text) => (
-  //           <ListItem href={URLS[text]} button key={text} component="a">
-  //             <ListItemIcon>{getIcon(text)}</ListItemIcon>
-  //             <ListItemText primary={text} />
-  //           </ListItem>
-  //         ))}
-  //       </List>
-  //     </Drawer>
-  //   </div>
-  // );
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" })
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Mini variant drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {navList.map(({ text, url, icon }) => (
+            <Link to={url}>
+              <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                <ListItemButton
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center"
+                    }}
+                  >
+                    {icon}
+                  </ListItemIcon>
+                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+    </Box>
+  );
 };
 
-export default withRouter(Header);
+export default withRouter(Nav);
